cfddeb16aa98c5892aa957388d76dcb3839f2dd7
caesarhu <hupeishun@gmail.com>
2020-12-05 08:41:50 +0800

    0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
 (defproject caesarhu/sql-formatter "0.1.0"
   :description "A Clojure port of great SQL formatter https://github.com/vertical-blank/sql-formatter."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/caesarhu/sql-formatter.git"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [["releases" :clojars
+                         :creds :gpg]
+                        ["snapshots" :clojars
+                         :creds :gpg]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.github.vertical-blank/sql-formatter "1.0.3"]]
   :repl-options {:init-ns sql-formatter.core})
